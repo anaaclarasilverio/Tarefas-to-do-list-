d4b853357d6a4019efbf58d1437115bbcc35dc8f
anaaclarasilverio <ds.46277728822@gmail.com>
2024-03-22 14:26:47 -0300

    projeto atualizado

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, TextInput } from "react-native";
+import { View, TextInput, Text } from "react-native";
 import Button from "./src/components/Button";
 import Task from "./src/components/Task";
 import styles from "./Global";
@@ -8,15 +8,30 @@ export default function App() {
 
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
-
+  const [error, setError] = useState("");
+  const [filter, setFilter] = useState("all");
+  const [filteredTasks, setFilteredTasks] = useState([]);
+  
   function addNewTasks(){
-    setTasks([...tasks, { text: newTask, completed: false}]);
+    setError("")
+    if (newTask !== "") {
+      setTasks([...tasks, { text: newTask, completed: false}]);
+    } else {
+      setError("Preencha a atividade")
+    }
+    
   }
 
   function toggleTask(index){
     const newTask =[...tasks]
-    newTask[index].completed = !newTask[index].completed;
-    setTasks(newTask)
+    const isTaskCompleted = newTask[index].completed;
+    if (isTaskCompleted) {
+      newTask[index].completed = false;
+    }else{
+      newTask[index].completed = true;
+    }
+    
+    setTasks(newTask);
   }
 
   useEffect(() =>{
@@ -29,12 +44,29 @@ export default function App() {
     setTasks(initialTasks);
 
   },[]);
+
+  useEffect(() => {
+    let result = tasks;
+    if (filter === 'completed') {
+      result = tasks.filter((tasks) => tasks.completed);
+    }else if (filter == "active") {
+      result = tasks.filter((tasks) => !tasks.completed);
+    }
+    setFilteredTasks(result);
+  },[tasks, filter]);
   
   return <View style={styles.container}>
     <TextInput value={newTask} style={styles.input} onChangeText={(text) => setNewTask(text)}/>  
+    {error && <Text style={{color:"red", marginBottom: 16, fontSize: 16}}>{error}</Text> }
     <Button title="Adicionar" onPress={() => addNewTasks()} />
+    <View style={styles.filterContainer}>
+      <Button title="Todas" selected={filter== "all"} onPress={() => setFilter("all")}/>
+      <Button title="Concluídas" selected={filter== "completed"}  onPress={() => setFilter ("completed")}/>
+      <Button title="Ativas" selected={filter== "active"}  onPress={() => setFilter ("active")}/>
+
+    </View>
     <View style={styles.listContainer}>
-      {tasks.map((task, index) => (
+      {filteredTasks.map((task, index) => (
         <Task key={task.text} isChecked={task.completed} label={task.text} onPress={() => toggleTask(index)} />
       ))}
       
